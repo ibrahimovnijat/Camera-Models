129b1de96a7ab39f5c865f43e7948fdb62cc9752
Nijat Ibrahimov <ibrahimovnijat@gmail.com>
2022-06-20 16:29:06 +0200

    camera models added

--- main.cpp
+++ main.cpp
@@ -4,28 +4,31 @@
 
 using namespace std;
 
-
 int main(){
-    // unique_ptr<CameraModel<double>> camModel = make_unique<PinholeCamera<double>>();    
-    shared_ptr<CameraModel<float>> camModel = make_shared<PinholeCamera<float>>();
+    
+    // declate camera parameters (also alpha/beta and k vals for other models)
+    CameraParams<double> params;
+    params.fx = 525;
+    params.fy = 525;
+    params.cx = 319.5;
+    params.cy = 239.5;
 
+    //e.g. 3D point in the camera coordinate
+    Matrix<double,3,1> point3d (3,5,1);
 
+    Matrix<double,2,1> pixel;
+    Matrix<double,3,1> p_reproj;
 
-    CameraParams<float> params;
-    params.fx = 200;
-    params.fy = 200;
-    params.cx = 250;
-    params.cy = 250;
-
+    // Crate an instance of camera with Pinhole model
+    unique_ptr<CameraModel<double>> camModel = make_unique<PinholeCamera<double>> ();
     camModel->initialize(params);
 
-    Matrix<float,3,1> vertex (5,4,2);
-    Matrix<float,2,1> pixels;
+    pixel = camModel->project(point3d);
+    p_reproj = camModel->unproject(pixel);
 
-    pixels = camModel->project(vertex);
-    
-    cout << "Pixel: \n" << pixels << endl;
-    cout << "Unproject: \n" << camModel->unproject(pixels) << endl;
+    cout << "Point: [" << point3d[0] << ", " << point3d[1] << ", " << point3d[2] << "]" << endl;
+    cout << "Pixel: [" << pixel[0] << ", " << pixel[1] << "]" << endl;
+    cout << "Reprojected point (normalized): [" <<p_reproj[0] << ", " << p_reproj[1] << ", " << p_reproj[2] << "]" << endl;
 
     return 0;
 }
--- models/camera_models.h
+++ models/camera_models.h
@@ -5,12 +5,23 @@
 
 using namespace Eigen;
 
+
 template <class T>
 struct CameraParams{
     T fx;
     T fy;
     T cx;
     T cy;
+    
+    T alpha;
+    T beta;
+    
+    T xi;
+
+    T k1;
+    T k2;
+    T k3;
+    T k4;
 };
 
 
@@ -22,14 +33,12 @@ public:
     virtual void initialize(const CameraParams<T>& params) = 0;
     virtual Matrix<T,2,1> project (const Matrix<T,3,1>& vertex) const = 0;
     virtual Matrix<T,3,1> unproject (const Matrix<T,2,1>& pixel) const = 0;
-
 };
 
 
 template<class T>  class PinholeCamera: public CameraModel<T> {
 private:
     T _fx, _fy, _cx, _cy;
-
 public:
     PinholeCamera(){
         std::cout << "PinholeCamera constructor called" << std::endl;
@@ -47,10 +56,10 @@ public:
         _cy = params.cy;
 
         std::cout <<"Pinhole camera model initialized!" << std::endl;
-        std::cout << "_fx = " << _fx << std::endl;
-        std::cout << "_fy = " << _fy << std::endl;
-        std::cout << "_cx = " << _cx << std::endl;
-        std::cout << "_cy = " << _cy << std::endl;
+        std::cout << "fx = " << _fx << std::endl;
+        std::cout << "fy = " << _fy << std::endl;
+        std::cout << "cx = " << _cx << std::endl;
+        std::cout << "cy = " << _cy << std::endl;
     }
     
     Matrix<T,2,1> project(const Matrix<T,3,1>& point) const {
@@ -64,37 +73,263 @@ public:
     }
 
     Matrix<T,3,1> unproject(const Matrix<T,2,1>& pixel) const {
-
+        
         Matrix<T, 3, 1> point3d = Matrix<T,3,1>::Zero();
         
         const T& mx = (pixel[0] - _cx) / _fx;
         const T& my = (pixel[1] - _cy) / _fy;
-        const T& m  = std::sqrt(mx * mx + my * my + 1);
+        const T&  m = std::sqrt(mx * mx + my * my + 1);
 
-        point3d = (1 / m) *  Matrix<T,3,1>(mx,my,1);
-        std::cout << "point3d = [" << point3d[0] << ", " << point3d[1] << ", " 
-                  << point3d[2] << "]" << std::endl;
-
+        point3d = (1 / m) * Matrix<T,3,1>(mx, my, 1);
         return point3d;
-    }   
-    
+    }
+
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 };
 
 
+template<class T> class ExtendedUnifiedCamera: public CameraModel<T> {
+private:
+    T _fx, _fy, _cx, _cy, _alpha, _beta;
+public:  
+    ExtendedUnifiedCamera() {};
+    virtual ~ExtendedUnifiedCamera() {};
+
+    void initialize(const CameraParams<T>& params) {
+        _fx = params.fx;
+        _fy = params.fy;
+        _cx = params.cx;
+        _cy = params.cy;
+        _alpha = params.alpha;
+        _beta  = params.beta;
+
+        std::cout << "Extended Unified Camera model initialized" << std::endl;
+        std::cout << "fx = " << _fx << std::endl;
+        std::cout << "fy = " << _fy << std::endl;
+        std::cout << "cx = " << _cx << std::endl;
+        std::cout << "cy = " << _cy << std::endl;
+        std::cout << "alpha = " << _alpha << std::endl;
+        std::cout << "beta  = " << _beta  << std::endl;
+    }
+
+    Matrix<T,2,1> project(const Matrix<T,3,1>& point) const {
+        
+        Matrix<T,2,1> pixel = Matrix<T,2,1>::Zero();
+
+        const T& x = point[0];
+        const T& y = point[1];
+        const T& z = point[2];
+
+        const T& d = std::sqrt(_beta * (x*x + y*y) + z*z);
+
+        pixel[0] = (_fx * x) / (_alpha * d + (1 - _alpha) * z) + _cx;
+        pixel[1] = (_fy * y) / (_alpha * d + (1 - _alpha) * z) + _cy;
+
+        return pixel;
+    }
+
+    Matrix<T,3,1> unproject(const Matrix<T,2,1>& pixel) const {
+
+        Matrix<T,3,1> point3d = Matrix<T,3,1>::Zero();
+        
+        const T& mx = (pixel[0] - _cx) / _fx;
+        const T& my = (pixel[1] - _cy) / _fy;
+
+        const T& r_squared = mx * mx + my * my;
+        const T& mz = (1 - _beta * _alpha * _alpha * r_squared) /
+                      (_alpha * std::sqrt(1 - (2 * _alpha -1) * _beta * r_squared) +
+                      (1 - _alpha));
+
+        point3d = 1 / std::sqrt(mx * mx + my * my + mz * mz) * Matrix<T,3,1>(mx, my, mz);
+
+        return point3d;
+    }
+
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+};
 
 
+template<class T> class DoubleSphereCamera: public CameraModel<T>{
+private:
+    T _fx, _fy, _cx, _cy, _xi, _alpha;
 
-// template<class T>
-// class ExtendedUnifiedCamera: public CameraModel<T> {
+public:
+    DoubleSphereCamera() = default;
+    virtual ~DoubleSphereCamera() = default;
+
+    void initialize(const CameraParams<T>& params) {
+
+        _fx = params.fx;
+        _fy = params.fy;
+        _cx = params.cx;
+        _cy = params.cy;
+        _xi = params.xi;
+        _alpha = params.alpha;
+
+        std::cout << "Double sphere camera model initialized!" << std::endl;
+        std::cout << "fx = " << _fx << std::endl;
+        std::cout << "fy = " << _fy << std::endl;
+        std::cout << "cx = " << _cx << std::endl;
+        std::cout << "cy = " << _cy << std::endl;
+        std::cout << "xi = " << _xi << std::endl;
+        std::cout << "alpha = " << _alpha << std::endl;
+    }
+
+    Matrix<T,2,1> project(const Matrix<T,3,1>& point) const {
+
+        Matrix<T,2,1> pixel = Matrix<T,2,1>::Zero();
+
+        const T& x = point[0];
+        const T& y = point[1];
+        const T& z = point[2];
+
+        const T& d1 = std::sqrt(x * x + y * y + z * z);
+        const T& d2 = std::sqrt(x * x + y * y + (_xi * d1 + z) * (_xi * d1 + z));
+
+        pixel[0] = (_fx * x) / (_alpha * d2 + (1 - _alpha) * (_xi * d1 + z)) + _cx;
+        pixel[1] = (_fy * y) / (_alpha * d2 + (1 - _alpha) * (_xi * d1 + z)) + _cy;
+
+        return pixel;
+    }
+
+    Matrix<T,3,1> unproject(const Matrix<T,2,1>& pixel) const {
+
+        Matrix<T,3,1> point3d = Matrix<T,3,1>::Zero();
+
+        const T& mx = (pixel[0] - _cx) / _fx;
+        const T& my = (pixel[1] - _cy) / _fy;
+
+        const T& r_squared = mx * mx + my * my;
+        const T& mz = (1 - _alpha * _alpha * r_squared) /
+                      (_alpha * std::sqrt(1 - (2 * _alpha - 1) * r_squared) + 1 - _alpha);
+
+        point3d = (mz * _xi + std::sqrt(mz * mz + (1 - _xi * _xi) * r_squared)) /
+                  (mz * mz + r_squared) * Matrix<T,3,1>(mx, my, mz) - Matrix<T,3,1>(0, 0, _xi);
+
+        return point3d;
+    }
+
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+};
+
+template<class T> class KannalaBrandtCamera: public CameraModel<T>{
+private:
+    T _fx, _fy, _cx, _cy, _k1, _k2, _k3, _k4;
+
+public:
+    KannalaBrandtCamera() = default;
+    virtual ~KannalaBrandtCamera() = default;
+
+    void initialize(const CameraParams<T>& params) {
+        _fx = params.fx;
+        _fy = params.fy;
+        _cx = params.cx;
+        _cy = params.cy;
+        _k1 = params.k1;
+        _k2 = params.k2;
+        _k3 = params.k3;
+        _k4 = params.k4;
+
+        std::cout << "Kannala Brandt camera model initialized!" << std::endl;
+        std::cout << "fx = " << _fx << std::endl;
+        std::cout << "fy = " << _fy << std::endl;
+        std::cout << "cx = " << _cx << std::endl;
+        std::cout << "cy = " << _cy << std::endl;
+        std::cout << "k1 = " << _k1 << std::endl;
+        std::cout << "k2 = " << _k2 << std::endl;
+        std::cout << "k3 = " << _k3 << std::endl;
+        std::cout << "k4 = " << _k4 << std::endl;
+
+    }
+
+    Matrix<T,2,1> project(const Matrix<T,3,1>& point) const {
+        
+        Matrix<T,2,1> pixel = Matrix<T,2,1>::Zero();
+
+        const T& x = point[0];
+        const T& y = point[1];
+        const T& z = point[2];
+
+        const T& eps = 1e-20;
+
+        const T& r = std::sqrt(x * x + y * y) + eps;
+        const T& theta = std::atan2(r, z);
+
+        T poly_val = 0;
+        Matrix<T,10,1> k;
+
+        k << _k4, 0, _k3, 0, _k2, 0, _k1, 0, 1, 0;
+
+        // calculate polynomial with Horner's method 
+        for (auto i = 0; i < 10; i++) {
+            poly_val = (poly_val * theta) + k[i];
+        }
+
+        pixel[0] = _fx * poly_val * x / r + _cx;
+        pixel[1] = _fy * poly_val * y / r + _cy;
+
+        return pixel;
+    }
     
-// };
+    Matrix<T,3,1> unproject(const Matrix<T,2,1>& pixel) const {
 
-// template<class T>
-// class DoubleSphereCamera: public CameraModel<T>{
+        Matrix<T,3,1> point3d = Matrix<T,3,1>::Zero();
 
-// };
+        const T& mx = (pixel[0] - _cx) / _fx;
+        const T& my = (pixel[1] - _cy) / _fy;
 
-// template<class T>
-// class KannalaBrandtCamera: public CameraModel<T>{
+        const T& eps = 1e-13;
 
-// };
+        const T& r = std::sqrt(mx * mx + my * my) + eps;
+
+        Matrix<T,10,1> k;
+        Matrix<T,9,1> k_prime;
+
+        k << _k4, 0, _k3, 0, _k2, 0, _k1, 0, 1, -r;
+        k_prime << 9 * _k4, 0, 7 * _k3, 0, 5 * _k2, 0, 3 * _k1, 0, 1;
+
+        //implement Newton's method to find roots
+        const T& iters = 10;
+        T theta_prev = 0.75;  // set initial value
+        bool converged = false;
+
+        T theta, poly_eval, poly_prime_eval;
+
+        for (auto i = 0; i < iters; i++) {
+            poly_eval = 0;
+            poly_prime_eval = 0;
+
+            for (auto i = 0; i < 10; i++) {
+                poly_eval = (poly_eval * theta_prev) + k[i];
+            }  
+
+            for (auto i = 0; i < 9; i++) {
+                poly_prime_eval = (poly_prime_eval * theta_prev) + k_prime[i];
+            }
+
+            theta = theta_prev - poly_eval / poly_prime_eval;
+
+            poly_eval = 0; 
+            for (auto i = 0; i < 10; i++) {
+                poly_eval = (poly_eval * theta) + k[i];
+            }
+
+            if (std::fabs(poly_eval) < 1e-7) {
+                converged = true;
+                break;
+            } else {
+                theta_prev = theta;
+            }
+        }
+
+        if (converged) {
+            point3d = Matrix<T,3,1> ( std::sin(theta) * mx / r , std::sin(theta) * my / r,
+                                      std::cos(theta) );
+        } else {
+            std::cout << "Newton's method did not converge!! Try either more iters or change initial value" << std::endl;
+        }
+        return point3d;
+    }
+
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+};
